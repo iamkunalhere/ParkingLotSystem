f84408507d7c7c497bf663bd1a482640fcd75b3c
iamkunalhere <iamkunalhere@gmail.com>
2020-04-29 01:02:56 +0530

    REFACTORED and MODIFIED: carParked function and added function to check that parking lot is full or not

--- main/ParkingLot.js
+++ main/ParkingLot.js
@@ -1,17 +1,33 @@
 const parkingLotOwner = require('../main/ParkingLotOwner');
 class ParkingLot {
+    constructor() {
+        this.counter = 0;
+        this.PARKING_LOT_CAPACITY = 3;
+    }
     // function to park the car
     carParked(carNumber,carName) {
+        if(this.isParkingLotFull()) {
+            throw new Error('parking lot is full');
+        }
         this.parkingLot = new Map();
         this.parkingLot.set(carNumber,carName);
-        let parkingOwner = new parkingLotOwner();
-        parkingOwner.parkingLotIsFull();
+        this.counter++;
         return true;
     }
     // function to unpark the car
     carUnParked(carNumber) {
         this.parkingLot.delete(carNumber);
+        this.counter--;
         return true;
+    }
+    // function to check parking lot is full or not
+    isParkingLotFull() {
+        if (this.counter == this.PARKING_LOT_CAPACITY) {
+            let parkingOwner = new parkingLotOwner();
+            parkingOwner.parkingLotIsFull();
+            return true;
+        }
+        return false;
     }
 }
 
